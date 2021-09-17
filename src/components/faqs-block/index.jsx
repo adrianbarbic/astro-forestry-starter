7ece1bec6605903a33b59cecbc309c5368707962
Adrian Barbic <adrian@inlight.com.au>
2021-09-18 08:09:47 +1000

    up

--- src/components/faqs-block/index.jsx
+++ src/components/faqs-block/index.jsx
@@ -7,7 +7,7 @@ const FaqsBlock = ({ content }) => (
           <div class="relative overflow-hidden text-white select-none">
             <h4 class="flex items-center justify-between py-5 text-lg font-medium text-gray-100 cursor-pointer sm:text-xl hover:text-white">
               <span>{item.question}</span>
-              <svg class="w-6 h-6 text-green-400 transition-all duration-200 ease-out transform rotate-0 fill-current" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+              <svg onClick={() => alert('hello')} class="w-6 h-6 text-green-400 transition-all duration-200 ease-out transform rotate-0 fill-current" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
             </h4>
             <p class="px-1 pt-0 mt-1 text-gray-300 sm:text-lg py-7">{item.answer}</p>
           </div>
