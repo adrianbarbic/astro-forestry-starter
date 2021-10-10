aef31db0c2de3affeaf969e749d772cff503f04e
Adrian Barbic <adrian@inlight.com.au>
2021-10-10 11:51:58 +1100

    up

--- src/components/team-block/index.jsx
+++ src/components/team-block/index.jsx
@@ -1,15 +1,17 @@
-import { head } from 'lodash';
+import { compact, isEmpty } from 'lodash';
 
 const TeamBlock = ({content, allProfiles}) => {
   const foo = allProfiles.filter(profile => {
-    console.log('profile: ', profile);
-    const out = content.staff.map(doc => {
-      if(profile.active === true && profile.file.pathname.includes(doc)){
-        return profile;
-      }
-      return false;
+    const out = content.staff.filter(doc => {
+      const profileFile = profile.file.pathname.split('src/')[1];
+      const contentFile = doc.split('src/')[1];
+
+      return profileFile === contentFile && profile.active === true;
     });
-    return out;
+
+    const output = isEmpty(out) ? null : out;
+
+    return output;
   });
 
   return(
@@ -38,7 +40,7 @@ const TeamBlock = ({content, allProfiles}) => {
                     </div>
                     <ul role="list" className="flex justify-center space-x-5">
                       <li>
-                        <a href={person.twitterUrl} className="text-gray-400 hover:text-gray-500">
+                        <a href={person.twitter_url} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">Twitter</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
@@ -46,7 +48,7 @@ const TeamBlock = ({content, allProfiles}) => {
                         </a>
                       </li>
                       <li>
-                        <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
+                        <a href={person.linkedin_url} className="text-gray-400 hover:text-gray-500">
                           <span className="sr-only">LinkedIn</span>
                           <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path
