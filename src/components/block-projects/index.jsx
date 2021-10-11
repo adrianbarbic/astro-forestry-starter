ea6cab301dccce81bd98cbfb49b03e3ea148e19d
Adrian Barbic <adrian@inlight.com.au>
2021-10-11 22:18:58 +1100

    up

--- src/components/block-projects/index.jsx
+++ src/components/block-projects/index.jsx
@@ -1,16 +1,26 @@
-const BlockProjects = ({content}) => (
-  <section className="box-border py-8 leading-7 text-gray-900 bg-white border-0 border-gray-200 border-solid sm:py-12 md:py-16 lg:py-24">
-    <div className="box-border max-w-6xl px-4 pb-12 mx-auto border-solid sm:px-6 md:px-6 lg:px-4">
-        <div className="flex flex-col items-center leading-7 text-center text-gray-900">
-            <h2 className="box-border m-0 text-3xl font-semibold leading-tight tracking-tight text-black border-solid sm:text-4xl md:text-5xl">
-                {content.heading}
-            </h2>
-            <p className="box-border mt-4 text-2xl leading-normal text-gray-900 border-solid">
-                We've got a plan for companies of any size
-            </p>
+const BlockProjects = ({content}) => {
+    console.log('content: ', content);
+    return(
+<div class="bg-white">
+  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+      
+      <a href="#" class="group">
+        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
         </div>
+        <h3 class="mt-4 text-sm text-gray-700">
+          Earthen Bottle
+        </h3>
+        <p class="mt-1 text-lg font-medium text-gray-900">
+          $48
+        </p>
+      </a>
+
     </div>
-  </section>
-  );
+  </div>
+</div>
+
+  )};
   
   export { BlockProjects };
