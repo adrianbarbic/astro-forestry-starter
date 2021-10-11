1117522f8ddb70f69aef3c26c31c12271bf8fd94
Adrian Barbic <adrian@inlight.com.au>
2021-10-11 23:43:58 +1100

    up

--- src/components/block-projects/index.jsx
+++ src/components/block-projects/index.jsx
@@ -1,26 +1,40 @@
-const BlockProjects = ({content}) => {
-    console.log('content: ', content);
+import { isEmpty } from 'lodash';
+
+const BlockProjects = ({content, allProjects}) => {
+    const projectsToShow = allProjects.filter(project => {
+      const out = content.projects.filter(doc => {
+        const projectFile = project.file.pathname.split('src/')[1];
+        const contentFile = doc.split('src/')[1];
+
+        const link = doc.replace('src/content/', '');
+        const newLink = link.replace('.md', '');
+
+        project.link = newLink;
+        return projectFile === contentFile && project.active === true;
+      });
+      const output = isEmpty(out) ? null : out;
+
+      return output;
+    });
+
+    console.log('projectsToShow: ', projectsToShow);
     return(
-<div class="bg-white">
-  <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-      
-      <a href="#" class="group">
-        <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-          <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+      <div class="bg-white">
+        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {projectsToShow.map(project => 
+            <a href={project.link} class="group">
+                <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                <img src={project.image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700">
+                {project.name}
+                </h3>
+            </a>
+            )}
+            </div>
+          </div>
         </div>
-        <h3 class="mt-4 text-sm text-gray-700">
-          Earthen Bottle
-        </h3>
-        <p class="mt-1 text-lg font-medium text-gray-900">
-          $48
-        </p>
-      </a>
-
-    </div>
-  </div>
-</div>
-
   )};
   
   export { BlockProjects };
