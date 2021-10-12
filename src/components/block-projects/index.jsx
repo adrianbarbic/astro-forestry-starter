8e9abecb78342d4ebbb479b4a2f5545942ce76d9
Adrian Barbic <adrian@inlight.com.au>
2021-10-12 17:39:18 +1100

    up

--- src/components/block-projects/index.jsx
+++ src/components/block-projects/index.jsx
@@ -7,6 +7,7 @@ const BlockProjects = ({content, allProjects}) => {
         const contentFile = doc.split('src/')[1];
 
         const link = doc.replace('src/content/', '');
+        console.log('link: ', link);
         const newLink = link.replace('.md', '');
 
         project.link = newLink;
@@ -17,13 +18,12 @@ const BlockProjects = ({content, allProjects}) => {
       return output;
     });
 
-    console.log('projectsToShow: ', projectsToShow);
     return(
       <div class="bg-white">
         <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {projectsToShow.map(project => 
-            <a href={project.link} class="group">
+            <a href={`/${project.link}`} class="group">
                 <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img src={project.image} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
                 </div>
