22625f9cddc93de6f8565d2efbe6e45a6d093284
Adrian Barbic <adrian@inlight.com.au>
2021-10-17 11:39:56 +1100

    up

--- src/components/block-header/index.tsx
+++ src/components/block-header/index.tsx
@@ -9,7 +9,13 @@ const BlockHeader = ({ content }: Content) => (
     <div className="absolute inset-0 w-full h-64 opacity-50 bg-gradient-to-b from-black to-transparent"></div>
 
     <div className="relative z-10 max-w-6xl px-10 py-40 mx-auto">
-      <div className="flex flex-col items-center h-full lg:flex-row">
+      <div
+        className={`flex flex-col items-center h-full lg:${
+          content.style.media_position === "Right"
+            ? "flex-row-reverse"
+            : "flex-row"
+        }`}
+      >
         <div className="flex flex-col items-center justify-center w-full h-full lg:w-2/3 lg:items-start">
           <p className="inline-block w-auto px-3 py-1 mb-5 text-xs font-medium text-white uppercase bg-green-700 rounded-full bg-gradient-to-br">
             {content.pill_text}
