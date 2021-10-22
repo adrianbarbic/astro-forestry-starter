201fb99f0b7090ab796372b86922524130469773
Adrian Barbic <adrian@inlight.com.au>
2021-10-22 23:57:58 +1100

    up

--- src/components/block-header/types.ts
+++ src/components/block-header/types.ts
@@ -2,8 +2,13 @@ export interface Content {
   content: Data,
 }
 
+export interface Style {
+  media_position: string,
+}
+
 export interface Data {
   heading:  string,
   pill_text: string,
   background_image: string,
+  style: Style,
 }
---
+++ src/utils/navigation-links.js
@@ -0,0 +1,9 @@
+const navigationLinks = (data, navigationName) => {
+  return data.navigation.map((links) => {
+    if (links.name === navigationName) {
+      return links.links;
+    }
+  });
+};
+
+export { navigationLinks };
