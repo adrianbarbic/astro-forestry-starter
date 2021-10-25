a62b9cc937e92663ba497b2935dedf521d8e961f
Adrian Barbic <adrian@inlight.com.au>
2021-10-25 22:55:16 +1100

    up

--- src/components/block-projects/index.jsx
+++ src/components/block-projects/index.jsx
@@ -22,7 +22,7 @@ const BlockProjects = ({ content, allProjects }) => {
       <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {projectsToShow.map((project) => (
-            <a href={`/${project.link}`} class="group">
+            <a href={`/projects/${project.uri}`} class="group">
               <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={project.image}
