2d8db735383c818ab69852b8550942a2d959f747
Adrian Barbic <adrian@inlight.com.au>
2021-10-30 23:24:09 +1100

    up

--- src/components/block-content-media/index.tsx
+++ src/components/block-content-media/index.tsx
@@ -1,30 +1,30 @@
-import { Content } from "./types";
+import { Content, defaultProps } from "./types";
 
 const BlockContentMedia = ({ content }: Content) => (
   <section className="relative flex flex-col items-center justify-center w-full px-6 py-24 bg-white bg-cover lg:py-32 min-w-screen">
     <div className="flex flex-col items-center justify-center mx-auto sm:p-6 xl:p-8 lg:flex-row lg:max-w-6xl lg:p-0">
       <div className="container relative z-20 flex flex-col w-full px-5 pb-1 pr-12 mb-16 text-2xl text-gray-700 lg:w-1/2 sm:pr-0 md:pr-6 md:pl-0 lg:pl-5 xl:pr-10 sm:items-center lg:items-start lg:mb-0">
-        <h1 className="relative z-20 text-5xl font-extrabold leading-none text-green-900 xl:text-6xl sm:text-center lg:text-left">
+        <h1 className="relative z-20 text-5xl font-extrabold leading-none text-gray-800 xl:text-6xl sm:text-center lg:text-left">
           {content.heading}
         </h1>
         <p className="relative z-20 block mt-6 text-base text-gray-500 xl:text-xl sm:text-center lg:text-left">
-          We've crafted the ultimate tool that helps you build the design of
-          your dreams. Built with configuration in mind, you can fully customize
-          every aspect.
+          {content.body}
         </p>
-        <div className="relative flex mt-4">
-          <a
-            href="#_"
-            className="flex items-center self-start justify-center px-5 py-2 mt-5 text-base font-medium leading-tight text-white transition duration-150 ease-in-out bg-green-700 border border-transparent rounded-full shadow lg:py-4 hover:bg-green-600 focus:outline-none focus:border-green-600 focus:shadow-outline-green md:text-lg xl:text-xl md:px-5 xl:px-10"
-          >
-            Get Started
-          </a>
-        </div>
+        {content.button_url && (
+          <div className="relative flex mt-4">
+            <a
+              href={content.button_url}
+              className="flex items-center self-start justify-center px-5 py-2 mt-5 text-base font-medium leading-tight text-white transition duration-150 ease-in-out bg-purple-500 border border-transparent rounded-full shadow lg:py-4 hover:bg-purple-600 focus:outline-none focus:border-purple-600 focus:shadow-outline-purple md:text-lg xl:text-xl md:px-5 xl:px-10"
+            >
+              {content.button_text}
+            </a>
+          </div>
+        )}
       </div>
       <div className="relative w-full px-5 rounded-lg cursor-pointer lg:w-1/2 group xl:px-0">
         <div className="absolute top-0 left-0 w-11/12 -mt-20 opacity-50">
           <svg
-            className="w-full h-full ml-4 text-green-100"
+            className="w-full h-full ml-4 text-purple-100"
             viewBox="0 0 200 200"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -36,25 +36,16 @@ const BlockContentMedia = ({ content }: Content) => (
           </svg>
         </div>
         <div className="relative overflow-hidden rounded-md shadow-2xl cursor-pointer group">
-          <div className="absolute flex items-center justify-center w-full h-full">
-            <span className="flex items-center justify-center w-20 h-20 bg-green-700 rounded-full shadow-2xl">
-              <svg
-                className="w-auto h-8 ml-1 text-white fill-current"
-                viewBox="0 0 52 66"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M50 30.7L4.1.6C2.6-.4.8.9.8 2.9v60.3c0 2 1.8 3.3 3.3 2.3L50 35.3c1.5-1 1.5-3.6 0-4.6z"
-                  fillRule="nonzero"
-                ></path>
-              </svg>
-            </span>
-          </div>
-          {/* <img src="https://images.unsplash.com/photo-1493857671505-72967e2e2760?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2850&amp;h=1603&amp;q=80" className="z-10 object-cover w-full h-full" alt="Placeholder" width="801" height="450" /> */}
+          <img
+            src="https://images.unsplash.com/photo-1493857671505-72967e2e2760?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2850&amp;h=1603&amp;q=80"
+            className="z-10 object-cover w-full h-full"
+          />
         </div>
       </div>
     </div>
   </section>
 );
+
+BlockContentMedia.defaultProps = defaultProps;
 
 export { BlockContentMedia };
--- src/components/block-content-media/types.ts
+++ src/components/block-content-media/types.ts
@@ -4,4 +4,15 @@ export interface Content {
 
 export interface Data {
   heading:  string,
+  body: string,
+  button_text: string,
+  button_url,
+}
+
+
+export const defaultProps: Data = {
+  heading:  '',
+  body: '',
+  button_text: 'Mr',
+  button_url: "http:"
 }
