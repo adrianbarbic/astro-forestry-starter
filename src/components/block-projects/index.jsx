f4a86530420e6fb94d34cb620de68e6dadbfa27f
Adrian Barbic <adrian@inlight.com.au>
2021-11-20 18:06:27 +1100

    update to classname

--- src/components/block-projects/index.jsx
+++ src/components/block-projects/index.jsx
@@ -18,21 +18,21 @@ const BlockProjects = ({ content, allProjects }) => {
   });
 
   return (
-    <div class="bg-white">
-      <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+    <div className="bg-white">
+      <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {projectsToShow.map((project) => (
-            <a href={`/projects/${project.uri}`} class="group">
-              <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+            <a href={`/projects/${project.uri}`} className="group">
+              <div className="w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={project.image}
                   alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                  class="w-full h-full object-center object-cover group-hover:opacity-75"
+                  className="object-cover object-center w-full h-full group-hover:opacity-75"
                   width="1200"
                   height="1200"
                 />
               </div>
-              <h3 class="mt-4 text-sm text-gray-700">{project.name}</h3>
+              <h3 className="mt-4 text-sm text-gray-700">{project.name}</h3>
             </a>
           ))}
         </div>
