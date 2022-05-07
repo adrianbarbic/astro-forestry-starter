cc275e22147f19f6ec866194fb7f4f3da4d33dc9
Adrian <adrianbarbic@gmail.com>
2022-05-07 21:57:57 +1000

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,7 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-import turbolinks from "@astrojs/turbolinks";
 
 export default defineConfig({
   site: "https://astro-forestry-starter.netlify.app",
-  integrations: [react(), turbolinks()],
+  integrations: [react()],
 });
